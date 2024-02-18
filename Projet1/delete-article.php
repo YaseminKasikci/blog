2306d044c34581c2d1a97d3af4fe5bdfef0935c6
YaseminKasikci <102510030+YaseminKasikci@users.noreply.github.com>
2024-02-18 11:09:34 +1100

    Update delete-article.php

--- Projet1/delete-article.php
+++ Projet1/delete-article.php
@@ -3,13 +3,14 @@ require_once __DIR__ . '/database/database.php';
 $authDB = require_once __DIR__ . '/database/security.php';
 $currentUser = $authDB->isLoggedin();
 
-//reccupe de l'id
+//get id
 if ($currentUser) {
   $articleDB = require_once __DIR__ . '/database/models/ArticleDB.php';
   $_GET = filter_input_array(INPUT_GET, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
   $id = $_GET['id'] ?? '';
 
-  // verification de l'utilisateur + autorisation à la suppression
+ 
+// user verification + authorization to delete
   if ($id) {
     $article = $articleDB->fetchOne($id);
     if ($article['author'] === $currentUser['id']) {
